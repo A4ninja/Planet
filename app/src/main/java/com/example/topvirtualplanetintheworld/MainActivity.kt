ce1b09a9a7059310cc873910738d01cd49e560ad
Alexandr A <Sizzzen@gmail.com>
2019-07-05 13:20:01 +0300

    end

--- app/src/main/java/com/example/topvirtualplanetintheworld/MainActivity.kt
+++ app/src/main/java/com/example/topvirtualplanetintheworld/MainActivity.kt
@@ -3,11 +3,17 @@ package com.example.topvirtualplanetintheworld
 import android.annotation.SuppressLint
 import android.net.Uri
 import android.os.Bundle
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import android.view.View
-import com.google.ar.core.*
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.core.Anchor
+import com.google.ar.core.HitResult
+import com.google.ar.core.Plane
+import com.google.ar.core.TrackingState
 import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.Node
+import com.google.ar.sceneform.Scene
 import com.google.ar.sceneform.math.Vector3
 import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
@@ -18,6 +24,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 @Suppress("SpellCheckingInspection")
 
 class MainActivity : AppCompatActivity() {
+    lateinit var scene: Scene
+    lateinit var earthNode: Node
     private lateinit var arFragment: ArFragment
     private var isTracking: Boolean = false
     private var isHitting: Boolean = false
@@ -37,6 +45,8 @@ class MainActivity : AppCompatActivity() {
             arFragment.onUpdate(frameTime)
             onUpdate()
         }
+        scene = sceneView.scene // get current scene
+        renderObject(Uri.parse("earth.sfb")) // Render the object
 
         // Set the onclick lister for our button
         // Change this string to point to the .sfb file of your choice :)
@@ -70,8 +80,8 @@ class MainActivity : AppCompatActivity() {
                 showFab(isHitting)
             }
         }
-
     }
+
 
     // Performs frame.HitTest and returns if a hit is detected
     private fun updateHitTest(): Boolean {
@@ -93,6 +103,16 @@ class MainActivity : AppCompatActivity() {
         return wasHitting != isHitting
     }
 
+//    override fun onPause() {
+//        super.onPause()
+//        sceneView.pause()
+//    }
+//
+//    override fun onResume() {
+//        super.onResume()
+//        sceneView.resume()
+//    }
+
     // Makes use of ARCore's camera state and returns true if the tracking state has changed
     private fun updateTracking(): Boolean {
         val frame = arFragment.arSceneView.arFrame
@@ -112,20 +132,20 @@ class MainActivity : AppCompatActivity() {
      *
      * This method takes in our 3D model and performs a hit test to determine where to place it
      */
-    private fun addObject(model: Uri) {
-        val frame = arFragment.arSceneView.arFrame
-        val point = getScreenCenter()
-        if (frame != null) {
-            val hits = frame.hitTest(point.x, point.y)
-            for (hit in hits) {
-                val trackable = hit.trackable
-                if (trackable is Plane && trackable.isPoseInPolygon(hit.hitPose)) {
-                    placeObject(arFragment, hit.createAnchor(), model)
-                    break
-                }
-            }
-        }
-    }
+//    private fun addObject(model: Uri) {
+//        val frame = arFragment.arSceneView.arFrame
+//        val point = getScreenCenter()
+//        if (frame != null) {
+//            val hits = frame.hitTest(point.x, point.y)
+//            for (hit in hits) {
+//                val trackable = hit.trackable
+//                if (trackable is Plane && trackable.isPoseInPolygon(hit.hitPose)) {
+//                    placeObject(arFragment, hit.createAnchor(), model)
+//                    break
+//                }
+//            }
+//        }
+//    }
 
     /**
      * @param fragment our fragment
@@ -135,17 +155,54 @@ class MainActivity : AppCompatActivity() {
      * Uses the ARCore anchor from the hitTest result and builds the Sceneform nodes.
      * It starts the asynchronous loading of the 3D model using the ModelRenderable builder.
      */
-    private fun placeObject(fragment: ArFragment, anchor: Anchor, model: Uri) {
+//    private fun placeObject(fragment: ArFragment, anchor: Anchor, model: Uri) {
+//        ModelRenderable.builder()
+//            .setSource(fragment.context, model)
+//            .build()
+//            .thenAccept {
+//                addNodeToScene(fragment, anchor, it)
+//            }
+//            .exceptionally {
+//                Toast.makeText(this@MainActivity, "Unexpected error", Toast.LENGTH_SHORT).show()
+//                return@exceptionally null
+//            }
+//    }
+    /** load the 3D model in the space
+    * @param parse - URI of the model, imported using Sceneform plugin
+    */
+    private fun renderObject(parse: Uri) {
         ModelRenderable.builder()
-            .setSource(fragment.context, model)
+            .setSource(this, parse)
             .build()
             .thenAccept {
-                addNodeToScene(fragment, anchor, it)
+                addNodeToScene(it)
             }
             .exceptionally {
-                Toast.makeText(this@MainActivity, "Unexpected error", Toast.LENGTH_SHORT).show()
+                val builder = AlertDialog.Builder(this)
+                builder.setMessage(it.message)
+                    .setTitle("error!")
+                val dialog = builder.create()
+                dialog.show()
                 return@exceptionally null
             }
+
+    }
+    /**
+     * Adds a node to the current scene
+     * @param model - rendered model
+     */
+    private fun addNodeToScene(model: ModelRenderable?) {
+        model?.let {
+            earthNode = Node().apply {
+                setParent(scene)
+                localPosition = Vector3(0f, 0f, -1f)
+                localScale = Vector3(3f, 3f, 3f)
+                name = "Earth"
+                renderable = it
+            }
+
+            scene.addChild(earthNode)
+        }
     }
 
     /**
